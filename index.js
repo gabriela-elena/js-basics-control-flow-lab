f3d65443aa36c15971b4b3c61fb8b330432b0faf
Gabriela <gabrielaelenadesigns@gmail.com>
2021-04-28 22:09:44 -0700

    commit tree 94639ded21fab997c02b523ea022fe7c7f5f71ac

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+  return "This one is on me!";
+    }
+  else if (1999 < feet && feet < 2500) {
+  return "I will gladly take your thirty bucks."
+    }
+  else {
+    return "No can do."
+  }
+};
+
+function ternaryCheckCity (city) {
+  return city=="NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip) {
+  if (tip == "generous") {
+    return 'Thank you so much.'
+  }
+  else if (tip == 'not as generous') {
+    return 'Thank you.'
+  }
+  else {
+    return 'Bye.'
+  }
+};
